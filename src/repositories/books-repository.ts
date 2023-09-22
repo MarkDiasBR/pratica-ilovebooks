32e96f592697837f085e2d82777211c4dcee3dde
MarkDiasBR <marcelo.dias@ufba.br>
2023-09-22 15:30:05 -0300

    refactor: using prisma orm

--- src/repositories/books-repository.ts
+++ src/repositories/books-repository.ts
@@ -1,44 +1,49 @@
 import { Book, CreateBook } from "../protocols/book";
 import { CreateReview } from "../protocols/review";
+import prisma from "../database/index";
 
 import connection from "../database";
 
-export async function getBooks() {
-  const query = `SELECT * FROM books`;
-  const result = await connection.query<Book>(query);
-  return result.rows;
+export async function getBooks(): Promise<Book[]> {
+  const books = await prisma.book.findMany();
+  return books;
 }
 
-export async function getBook(id: number) {
-  const query = `SELECT * FROM books WHERE id = $1`;
-  const result = await connection.query<Book>(query, [id]);
-  return result.rows[0];
+export async function getBook(id: number): Promise<Book> {
+  const book = await prisma.book.findUnique({
+    where : { id }
+  })
+  return book;
 }
 
 export async function createBook(book: CreateBook) {
-  const { title, author, publisher, purchaseDate } = book;
-  const query = `
-    INSERT INTO books (title, author, publisher, "purchaseDate")
-    VALUES ($1, $2, $3, $4)`;
-
-  const result = await connection.query(query, [
-    title, author, publisher, purchaseDate
-  ]);
-
-  return result.rowCount;
+  return await prisma.book.create({
+    data : book
+  });
 }
 
 export async function reviewBook(bookReview: CreateReview) {
-  const { bookId, grade, review } = bookReview;
-  const query = `
-    UPDATE books 
-    SET
-      grade = $1,
-      review = $2,
-      read = true 
-    WHERE id = $3
-  `;
+  return await prisma.book.update({
+    where: {
+      id: bookReview.bookId
+    },
+    data: {
+      grade: bookReview.grade,
+      review: bookReview.review,
+      read: true
+    }
+  })
 
-  const result = await connection.query(query, [grade, review, bookId]);
-  return result.rowCount;
+  // const { bookId, grade, review } = bookReview;
+  // const query = `
+  //   UPDATE books 
+  //   SET
+  //     grade = $1,
+  //     review = $2,
+  //     read = true 
+  //   WHERE id = $3
+  // `;
+
+  // const result = await connection.query(query, [grade, review, bookId]);
+  // return result.rowCount;
 }
